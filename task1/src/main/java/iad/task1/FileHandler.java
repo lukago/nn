cba75fb7760ddd02569df1bfe47f1fd56406f9a3
glbwsk <golebiewski96@gmail.com>
2017-04-08 18:21:08 +0200

    tex #2

--- task1/src/main/java/iad/task1/FileHandler.java
+++ task1/src/main/java/iad/task1/FileHandler.java
@@ -240,4 +240,42 @@ public final class FileHandler {
 			e.printStackTrace();
 		}
 	}
+
+	public static void compareImg(String imgS1, String imgS2) {
+		BufferedImage img1 = null;
+		BufferedImage img2 = null;
+		try {
+			img1 = ImageIO.read(new File(imgS1));
+			img2 = ImageIO.read(new File(imgS2));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int width1 = img1.getWidth(null);
+		int width2 = img2.getWidth(null);
+		int height1 = img1.getHeight(null);
+		int height2 = img2.getHeight(null);
+		if ((width1 != width2) || (height1 != height2)) {
+			System.err.println("Error: Images dimensions mismatch");
+			System.exit(1);
+		}
+		long diff = 0;
+		for (int y = 0; y < height1; y++) {
+			for (int x = 0; x < width1; x++) {
+				int rgb1 = img1.getRGB(x, y);
+				int rgb2 = img2.getRGB(x, y);
+				int r1 = (rgb1 >> 16) & 0xff;
+				int g1 = (rgb1 >> 8) & 0xff;
+				int b1 = (rgb1) & 0xff;
+				int r2 = (rgb2 >> 16) & 0xff;
+				int g2 = (rgb2 >> 8) & 0xff;
+				int b2 = (rgb2) & 0xff;
+				diff += Math.abs(r1 - r2);
+				diff += Math.abs(g1 - g2);
+				diff += Math.abs(b1 - b2);
+			}
+		}
+		double n = width1 * height1 * 3;
+		double p = diff / n / 255.0;
+		System.out.println("diff percent: " + (p * 100.0));
+	}
 }
--- task1/src/main/java/iad/task1/TaskOneApp.java
+++ task1/src/main/java/iad/task1/TaskOneApp.java
@@ -7,9 +7,46 @@ public class TaskOneApp {
 
 	public static void main(String[] args) {
 		
-		images();	
+		//images();	
 		//txt();
-
+		//float p = FileHandler.compareImage(new File("images/lenakolor3.jpg"), new File("results_km/out.bmp") );
+		//System.out.println(p);
+		
+		//n = 20, iter = 20 fs =3
+		//FileHandler.compareImg("images/lenakolor3.jpg", "results_km/out.png");
+		
+		//int n = 20; iter = 2000; mapRadius = 50; lr = 0.05; double lrc = 1;
+		//FileHandler.compareImg("images/lenakolor3.jpg", "results_ng/out.png");
+		
+		//int n = 100; iter = 2000; mapRadius = 1; lr = 0.05; double lrc = 1;	
+		FileHandler.compareImg("images/lenakolor3.jpg", "results_khn/out.png");
+	}
+	
+	public static void images() {
+		FileHandler.makeEmptyDir("data_img");
+		String imageFile = "images/lenakolor3.jpg";
+		String imageData = "data_img/img.data";
+		String sep = "\t";
+		int frameSz = 3;
+		int n = 20;
+		int iter = 1000;
+		double mapRadius = 1;
+		double lr = 0.1;
+		double lrc = 1;	
+		
+		FileHandler.parseIMG(imageFile, imageData, sep, frameSz);
+		
+		//KMeans k = new KMeans(n, iter, imageData, sep, false);
+		//k.calc(false, null, null);
+		
+		Kohonen k = new Kohonen(n, iter, imageData, sep, false, mapRadius, lr, lrc); 
+		k.calc(false, null, null);
+		
+		//NeuralGas k = new NeuralGas(n, iter, imageData, sep, false, mapRadius, lr, lrc); 
+		//k.calc(false, null, null);
+		
+		FileHandler.writeMatrixToImage(FileHandler.readPixels(k.destDir + k.imgcprFile, sep), 
+				imageFile, k.destDir+"out.png", sep, frameSz, "png");
 	}
 	
 	public static void txt() {
@@ -19,34 +56,7 @@ public class TaskOneApp {
 		//Neural neural = new Kohonen(6, 1000, "data/sample1.data", "\t", false, 1, 0.1, 1); 
 		//neural.calc(true, "1 2 3", "gnuplot/plot_khn.gpt");
 
-		Neural neural = new NeuralGas(6, 1000, "data/sample1.data", "\t", false, 1, 0.1, 1); 
-		neural.calc(true, "1 2 3", "gnuplot/plot_ng.gpt");
-	}
-	
-	public static void images() {
-		FileHandler.makeEmptyDir("data_img");
-		String imageFile = "images/lena.bmp";
-		String imageData = "data_img/img.data";
-		String sep = "\t";
-		int frameSz = 4;
-		int n = 20;
-		int iter = 500;
-		double mapRadius = 100;
-		double lr = 0.1;
-		double lrc = 1;	
-		
-		FileHandler.parseIMG(imageFile, imageData, sep, frameSz);
-		
-		//KMeans k = new KMeans(n, iter, imageData, sep, false);
-		//k.calc(false, null, null);
-		
-		//Kohonen k = new Kohonen(n, iter, imageData, sep, false, mapRadius, lr, lrc); 
-		//k.calc(false, null, null);
-		
-		NeuralGas k = new NeuralGas(n, iter, imageData, sep, false, mapRadius, lr, lrc); 
-		k.calc(false, null, null);
-		
-		FileHandler.writeMatrixToImage(FileHandler.readPixels(k.destDir + k.imgcprFile, sep), 
-				imageFile, k.destDir+"out.bmp", sep, frameSz, "bmp");
+		//Neural neural = new NeuralGas(6, 1000, "data/sample1.data", "\t", false, 1, 0.1, 1); 
+		//neural.calc(true, "1 2 3", "gnuplot/plot_ng.gpt");
 	}
 }
