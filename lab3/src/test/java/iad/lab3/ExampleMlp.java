d09691b5a4e1a48b66fcee32fd015cde2b836c67
glbwsk <golebiewski96@gmail.com>
2017-05-03 18:31:04 +0200

    input example

--- lab3/src/test/java/iad/lab3/ExampleMlp.java
+++ lab3/src/test/java/iad/lab3/ExampleMlp.java
@@ -1,5 +1,6 @@
 package iad.lab3;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,43 +8,48 @@ import java.util.List;
 import iad.lab3.activationFunctions.Sigmoidal;
 import iad.lab3.utlis.MLPUtils;
 
-public class Xor {
+public class ExampleMlp {
 
 	public static void main(String[] args) {
-		int[] layers = new int[] { 2, 6, 1 };
-		MultiLayerPerceptron net = new MultiLayerPerceptron(layers, 0.2, true, new Sigmoidal());
 		
+		//initalize mlp
+		int[] layers = new int[] { 2, 6, 1 };
+		double learningRate = 0.2;
+		boolean useBias = true;
+		ActivationFunction f = new Sigmoidal();
+		int epochs = 1000;
 		String in = "data/xor_in.data";
 		String out = "data/xor_out.data";
+		String cmd = "gnuplot -c " + System.getProperty("user.dir") + "/gnuplot/plot.gpt " + out;
+		
+		MultiLayerPerceptron mlp = new MultiLayerPerceptron(layers, learningRate, useBias, f);
+		
+		//local 
+		double[] epochOut;
 		double[][] inputs = MLPUtils.readMatrix(in, "\t");
-		double[][] outputs = MLPUtils.readMatrix(out, "\t");
+		double[][] outputs = MLPUtils.readMatrix(out, "\t");	
+		double[] quadFunVals = new double[epochs];
 		List<Integer> indexes = new ArrayList<>();
 		for (int i = 0; i<outputs.length; i++) {
 			indexes.add(i);
-		}
-
-		/* Learning */
-		int epochs = 1000;
-		double[] quadFunVals = new double[epochs];
-		double[] epochOut;
+		}	
+		
 		for (int i = 0; i < epochs; i++) {
 			Collections.shuffle(indexes);
 			for (int j = 0; j < inputs.length; j++) {
-				epochOut = net.backPropagate(inputs[indexes.get(j)], outputs[indexes.get(j)]);
+				epochOut = mlp.backPropagate(inputs[indexes.get(j)], outputs[indexes.get(j)]);
 				quadFunVals[i] = MLPUtils.quadraticCostFun(epochOut, outputs[indexes.get(j)]);
 			}
 		}
-
+		
 		double[][] outFinal = new double [inputs.length][];
 		for (int i = 0; i < inputs.length; i++) {
-			outFinal[i] = net.execute(inputs[i]);
+			outFinal[i] = mlp.execute(inputs[i]);
 		}
-		
-		// write results to plot
+
+		new File("results").mkdir();
 		MLPUtils.writeQuadFun("results/quad.data", quadFunVals);
-		MLPUtils.writeResults("results/out.data", outFinal);
-		
-		String cmd = "gnuplot -c " + System.getProperty("user.dir") + "/gnuplot/plot.gpt " + out;
+		MLPUtils.writeResults("results/out.data", outFinal);	
 		MLPUtils.rumCmd(cmd);
 	}
 }
